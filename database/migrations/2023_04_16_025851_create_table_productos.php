1d3c60968295bc90e07b8ac9dd9809ea75260c4e
Natalie <ngomez.cep@gmail.com>
2023-04-16 05:34:46 +0200

    Tablas creadas

---
+++ database/migrations/2023_04_16_025851_create_table_productos.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableProductos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 255);
+            $table->text('descripcion');
+            $table->decimal('precio', 10, 2);
+            $table->unsignedBigInteger('proveedor_id');
+            $table->foreign('proveedor_id')->references('id')->on('proveedores');
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_productos');
+    }
+}
