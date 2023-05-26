8e3f369c9ff51417bf7cb1c10c8f863e76b09045
Natalie Gomez Tapia <natalie.gomez@ad.avancemgrup.com>
2023-05-26 10:57:46 +0200

    purc

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PurchasesController;
 use App\Http\Controllers\Api\InfUserController;
 use App\Http\Controllers\Api\ProductoController;
 use App\Http\Controllers\Api\ProveedorController;
