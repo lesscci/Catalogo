9aec9a7dee79d0ef5b094c5efd966d8092731893
Natalie Gomez Tapia <natalie.gomez@ad.avancemgrup.com>
2023-05-26 11:23:58 +0200

    v

--- app/Http/Controllers/Api/PurchasesController.php
+++ app/Http/Controllers/Api/PurchasesController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Purcharse;
+use App\Models\Purchase;
 use Illuminate\Http\Request;
 
 class PurchasesController extends Controller
